3452184814e0b3c1fd303e53540f9004051aac31
enoutowa <miyu.run.0220@gmail.com>
2026-09-08 11:54:46 +0900

    Add files via upload

---
+++ news-data.js
@@ -0,0 +1,21 @@
+const newsItems = [
+
+  {
+    date: "2026.09.08",
+    text: "BLOGを更新しました。",
+    url: "./article.html?id=20260908-auto"
+  },
+
+  {
+    date: "2026.09.07",
+    text: "新曲『恋愛抗争』をYouTubeにアップロードしました。",
+    url: "https://youtu.be/NC9w21RSoRg?si=TmbFSpfr_SqeR1lG"
+  },
+
+  {
+    date: "2026.09.06",
+    text: "新曲『我流』をYouTubeにアップロードしました。",
+    url: "https://youtu.be/KDGYMUREle0?si=N5VWE5vHGlDFaeIB"
+  }
+
+];
