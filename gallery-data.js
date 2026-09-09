78b28d49be926650dae5542be54d2e8d8dc25057
enoutowa <miyu.run.0220@gmail.com>
2026-09-09 12:22:31 +0900

    Add files via upload

---
+++ gallery-data.js
@@ -0,0 +1,45 @@
+const galleryItems = [
+
+  {
+    id: "gallery07",
+    image: "./images/gallery07.png",
+    alt: "鴛鴦トワ Gallery 07"
+  },
+
+  {
+    id: "gallery06",
+    image: "./images/gallery06.png",
+    alt: "鴛鴦トワ Gallery 06"
+  },
+
+  {
+    id: "gallery05",
+    image: "./images/gallery05.png",
+    alt: "鴛鴦トワ Gallery 05"
+  },
+
+  {
+    id: "gallery04",
+    image: "./images/gallery04.png",
+    alt: "鴛鴦トワ Gallery 04"
+  },
+
+  {
+    id: "gallery03",
+    image: "./images/gallery03.png",
+    alt: "鴛鴦トワ Gallery 03"
+  },
+
+  {
+    id: "gallery02",
+    image: "./images/gallery02.png",
+    alt: "鴛鴦トワ Gallery 02"
+  },
+
+  {
+    id: "gallery01",
+    image: "./images/gallery01.png",
+    alt: "鴛鴦トワ Gallery 01"
+  }
+
+];
