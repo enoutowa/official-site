d8acc3c47f435663637a5d22f59a412a8b33951d
enoutowa <miyu.run.0220@gmail.com>
2026-09-09 12:34:43 +0900

    Add files via upload

--- gallery-data.js
+++ gallery-data.js
@@ -1,81 +1,15 @@
 const galleryItems = [
-
-{
-  id: "gallery13",
-  image: "./images/gallery13.png",
-  alt: "鴛鴦トワ Gallery 13"
-},
-
-{
-  id: "gallery12",
-  image: "./images/gallery12.png",
-  alt: "鴛鴦トワ Gallery 12"
-},
-
-{
-  id: "gallery11",
-  image: "./images/gallery11.png",
-  alt: "鴛鴦トワ Gallery 11"
-},
-
-{
-  id: "gallery10",
-  image: "./images/gallery10.png",
-  alt: "鴛鴦トワ Gallery 10"
-},
-
-{
-  id: "gallery09",
-  image: "./images/gallery09.png",
-  alt: "鴛鴦トワ Gallery 09"
-},
-
-{
-  id: "gallery08",
-  image: "./images/gallery08.png",
-  alt: "鴛鴦トワ Gallery 08"
-},
-
-  {
-    id: "gallery07",
-    image: "./images/gallery07.png",
-    alt: "鴛鴦トワ Gallery 07"
-  },
-
-  {
-    id: "gallery06",
-    image: "./images/gallery06.png",
-    alt: "鴛鴦トワ Gallery 06"
-  },
-
-  {
-    id: "gallery05",
-    image: "./images/gallery05.png",
-    alt: "鴛鴦トワ Gallery 05"
-  },
-
-  {
-    id: "gallery04",
-    image: "./images/gallery04.png",
-    alt: "鴛鴦トワ Gallery 04"
-  },
-
-  {
-    id: "gallery03",
-    image: "./images/gallery03.png",
-    alt: "鴛鴦トワ Gallery 03"
-  },
-
-  {
-    id: "gallery02",
-    image: "./images/gallery02.png",
-    alt: "鴛鴦トワ Gallery 02"
-  },
-
-  {
-    id: "gallery01",
-    image: "./images/gallery01.png",
-    alt: "鴛鴦トワ Gallery 01"
-  }
-
+  "gallery13.png",
+  "gallery12.png",
+  "gallery11.png",
+  "gallery10.png",
+  "gallery09.png",
+  "gallery08.png",
+  "gallery07.png",
+  "gallery06.png",
+  "gallery05.png",
+  "gallery04.png",
+  "gallery03.png",
+  "gallery02.png",
+  "gallery01.png"
 ];
